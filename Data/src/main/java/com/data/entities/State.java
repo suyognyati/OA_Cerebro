814775456065341622b6b15356016d96ecb6d542
Suyog <suyognyati@gmail.com>
2016-08-28 15:25:47 +0530

    Added state entity

--- Data/src/main/java/com/data/entities/State.java
+++ Data/src/main/java/com/data/entities/State.java
@@ -1,5 +1,34 @@
 package com.data.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="States")
 public class State {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int StateId;
+	private String StateName;
+	
+	//StateId
+	public int getStateId() {
+		return StateId;
+	}
+	public void setStateId(int stateId) {
+		StateId = stateId;
+	}
+	
+	//StateName
+	public String getStateName() {
+		return StateName;
+	}
+	public void setStateName(String stateName) {
+		StateName = stateName;
+	}
+	
 }
