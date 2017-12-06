7aba9cfe9243b0e1678d5752454775a31b43d538
Prachi <prachi.javdekar@gmail.com>
2017-12-06 22:23:19 +0530

    Updated Applicant detail UI

--- Web/src/main/webapp/studentview/app/controllers/applicantDetailCtrl.js
+++ Web/src/main/webapp/studentview/app/controllers/applicantDetailCtrl.js
@@ -14,21 +14,31 @@
 	function ApplicantDetailCtrl($state, $http, $httpParamSerializer, applicantDetailService, $scope, $window) {
 		var userId = 1;
 		var vm = this;
-		vm.personalDetailTemp = {};
+		vm.indianCitizenship = true;
+		vm.applicantdetailTemp = {};
 		
 		applicantDetailService.setApplicant(userId)
 		.success(function (data, status, headers, config) {
 			applicantDetailService.getApplicant()
 			.success(function (data, status, headers, config) {
-				vm.personaldetail = data;
+				vm.applicantdetail = data;
+				vm.initModel();
 			})
 			.error(function (data, status, headers, config) {
-				vm.personaldetail = {};
+				vm.applicantdetail = {};
 			});
 		})
 		.error(function (data, status, headers, config) {
-			vm.personaldetail = {};
+			vm.applicantdetail = {};
 		});
+		
+		vm.initModel = function() {
+			if(vm.applicantdetail.countryOfCitizenship.countryName == "India") {
+				vm.indianCitizenship = true
+			} else {
+				vm.indianCitizenship = false;
+			}
+		}
 	};
 	
 }());
